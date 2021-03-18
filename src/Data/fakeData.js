fac334a438cab00c60a93b84560258da990ef270
Samin1615088 <samin.shafkat@gmail.com>
2021-03-19 02:01:28 +0600

    fakeData added and tested

---
+++ src/Data/fakeData.js
@@ -0,0 +1,107 @@
+const fakeData = {
+    "bike": [
+        {
+            "key": "001",
+            "capacity": "1",
+            "image": "https://i.ibb.co/rHkqQh2/Frame.png",
+            "rent": "$63"
+        },
+        {
+            "key": "002",
+            "capacity": "1",
+            "image": "https://i.ibb.co/rHkqQh2/Frame.png",
+            "rent": "$47"
+        },
+        {
+            "key": "003",
+            "capacity": "1",
+            "image": "https://i.ibb.co/rHkqQh2/Frame.png",
+            "rent": "$95"
+        },
+        {
+            "key": "004",
+            "capacity": "1",
+            "image": "https://i.ibb.co/rHkqQh2/Frame.png",
+            "rent": "$112"
+        }
+    ],
+    "car": [
+        {
+            "key": "005",
+            "capacity": "4",
+            "image": "https://i.ibb.co/GvPtJ9X/Frame-2.png",
+            "rent": "$350"
+        },
+        {
+            "key": "006",
+            "capacity": "4",
+            "image": "https://i.ibb.co/GvPtJ9X/Frame-2.png",
+            "rent": "$920"
+        },
+        {
+            "key": "007",
+            "capacity": "4",
+            "image": "https://i.ibb.co/GvPtJ9X/Frame-2.png",
+            "rent": "$112"
+        },
+        {
+            "key": "008",
+            "capacity": "4",
+            "image": "https://i.ibb.co/GvPtJ9X/Frame-2.png",
+            "rent": "$741"
+        }
+    ],
+    "bus": [
+        {
+            "key": "009",
+            "capacity": "30",
+            "image": "https://i.ibb.co/DGPYmKx/Frame-1.png",
+            "rent": "$20"
+        },
+        {
+            "key": "010",
+            "capacity": "30",
+            "image": "https://i.ibb.co/DGPYmKx/Frame-1.png",
+            "rent": "$19"
+        },
+        {
+            "key": "011",
+            "capacity": "30",
+            "image": "https://i.ibb.co/DGPYmKx/Frame-1.png",
+            "rent": "$4"
+        },
+        {
+            "key": "012",
+            "capacity": "30",
+            "image": "https://i.ibb.co/DGPYmKx/Frame-1.png",
+            "rent": "$13"
+        }
+    ],
+    "train": [
+        {
+            "key": "013",
+            "capacity": "100",
+            "image": "https://i.ibb.co/ZGK19Z4/Group.png",
+            "rent": "$2"
+        },
+        {
+            "key": "014",
+            "capacity": "100",
+            "image": "https://i.ibb.co/ZGK19Z4/Group.png",
+            "rent": "$12"
+        },
+        {
+            "key": "015",
+            "capacity": "100",
+            "image": "https://i.ibb.co/ZGK19Z4/Group.png",
+            "rent": "$10"
+        },
+        {
+            "key": "016",
+            "capacity": "100",
+            "image": "https://i.ibb.co/ZGK19Z4/Group.png",
+            "rent": "$3"
+        }
+    ]
+};
+export default fakeData;
