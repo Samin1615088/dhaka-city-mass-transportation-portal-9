8ed005997c0134fe8948be3fe82563e8875af690
Samin1615088 <samin.shafkat@gmail.com>
2021-03-19 07:51:54 +0600

    bootstrap import updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import data from './Data/fakeData';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const fakeData = data;
