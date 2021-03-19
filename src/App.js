199438e549e39785bba9232b31975d693b99e5da
Samin1615088 <samin.shafkat@gmail.com>
2021-03-19 09:53:50 +0600

    basic router added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,61 @@ import './App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { Router, Switch, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Search from './components/Search/Search';
+import NotFound from './components/NotFound/NotFound';
 
 
 function App() {
 
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <div>
+          {/* <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav> */}
+          <Header />
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/home">
+              <Home />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/search">
+              <Search />
+            </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </ div >
   );
 }
