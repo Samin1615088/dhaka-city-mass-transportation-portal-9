3dd310f27119679831c61bf7716e35e40826cb7f
Samin1615088 <samin.shafkat@gmail.com>
2021-03-19 07:44:19 +0600

    fake data display removed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import data  from './Data/fakeData';
+import data from './Data/fakeData';
 
 function App() {
   const fakeData = data;
   return (
     <div>
-      {
-        fakeData.bike.map( item => <li>{item.key}</li>)
-      }
+
     </div>
   );
 }
